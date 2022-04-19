48cdaa65f00799afb3ae601308c9114f07dd11e4
Yuxiang Cao <caoyxsh@outlook.com>
2022-04-18 19:26:21 -0700

    add upgrade

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,18 +1,84 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
+import "./@rarible/royalties/contracts/impl/RoyaltiesV2Impl.sol";
+import "./@rarible/royalties/contracts/LibPart.sol";
+import "./@rarible/royalties/contracts/LibRoyaltiesV2.sol";
 
-contract NFT is ERC721PresetMinterPauserAutoId {
-    /// @dev Base token URI used as a prefix by tokenURI().
-    string public baseTokenURI;
+contract NFT is
+    ERC721EnumerableUpgradeable,
+    OwnableUpgradeable,
+    RoyaltiesV2Impl
+{
+    using CountersUpgradeable for CountersUpgradeable.Counter;
+    CountersUpgradeable.Counter private _tokenIdTracker;
+    bytes4 private constant _INTERFACE_ID_ERC2981 = 0x2a55205a;
 
-    constructor()
-        ERC721PresetMinterPauserAutoId(
-            "TestEggNFT",
-            "EggNFT",
-            "https://bafybeid5vki6m7cefh2m36nk666vcq3uvufc2ept36ljja5dj5fpwdtk5y.ipfs.nftstorage.link/metadata/"
-        )
-    {}
+    function initialize() public initializer {
+        __ERC721_init("TestEggNFT", "EggNFT");
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return
+            "https://bafybeid5vki6m7cefh2m36nk666vcq3uvufc2ept36ljja5dj5fpwdtk5y.ipfs.nftstorage.link/metadata/";
+    }
+
+    function mint(address to) public onlyOwner {
+        // We cannot just use balanceOf to create the new tokenId because tokens
+        // can be burned (destroyed), so we need a separate counter.
+        _mint(to, _tokenIdTracker.current());
+        _tokenIdTracker.increment();
+    }
+
+    /**
+     * @dev Rarible works by using an on-chain OrderMatcher, which internally queries the token for secondary sales royalties,
+     *      here we mimic the interface, so that the Rarible DAO gets the right information!
+     * @param _tokenId The id of token to be set royalties
+     * @param _royaltiesReceipientAddress The address where royalties will be paied to
+     * @param _percentageBasisPoints The Percentage is given in percentage basis points. That means, 2.5% is 250
+     */
+    function setRoyalties(
+        uint256 _tokenId,
+        address payable _royaltiesReceipientAddress,
+        uint96 _percentageBasisPoints
+    ) public onlyOwner {
+        LibPart.Part[] memory _royalties = new LibPart.Part[](1);
+        _royalties[0].value = _percentageBasisPoints;
+        _royalties[0].account = _royaltiesReceipientAddress;
+        _saveRoyalties(_tokenId, _royalties);
+    }
+
+    function royaltyInfo(uint256 _tokenId, uint256 _salePrice)
+        external
+        view
+        returns (address receiver, uint256 royaltyAmount)
+    {
+        LibPart.Part[] memory _royalties = royalties[_tokenId];
+        if (_royalties.length > 0) {
+            return (
+                _royalties[0].account,
+                (_salePrice * _royalties[0].value) / 10000
+            );
+        }
+        return (address(0), 0);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC721EnumerableUpgradeable)
+        returns (bool)
+    {
+        if (interfaceId == LibRoyaltiesV2._INTERFACE_ID_ROYALTIES) {
+            return true;
+        }
+        if (interfaceId == _INTERFACE_ID_ERC2981) {
+            return true;
+        }
+        return super.supportsInterface(interfaceId);
+    }
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,4 @@
 import * as dotenv from "dotenv";
-
 import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
@@ -7,15 +6,15 @@ import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
 import "@nomiclabs/hardhat-ethers";
+import "@openzeppelin/hardhat-upgrades";
 import { getAccount, getContract } from "./scripts/helpers";
 import fetch from "node-fetch";
 
 dotenv.config();
 
-const { ALCHEMY_KEY, ACCOUNT_PRIVATE_KEY } = process.env;
+const { ALCHEMY_KEY, ACCOUNT_PRIVATE_KEY, NFT_CONTRACT_ADDRESS } = process.env;
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
+// Tasks begin
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -33,7 +32,7 @@ task("check-balance", "Prints out the balance of your account").setAction(
   }
 );
 
-task("deploy", "Deploys the NFT.sol contract").setAction(async function (
+task("deploy", "Creates the NFT.sol contract").setAction(async function (
   taskArguments,
   hre
 ) {
@@ -41,7 +40,25 @@ task("deploy", "Deploys the NFT.sol contract").setAction(async function (
     "NFT",
     getAccount()
   );
-  const nft = await nftContractFactory.deploy();
+  const nft = await hre.upgrades.deployProxy(nftContractFactory);
+  console.log(`Contract deployed to address: ${nft.address}`);
+});
+
+task("upgrade", "Upgrades the NFT.sol contract").setAction(async function (
+  taskArguments,
+  hre
+) {
+  if (!NFT_CONTRACT_ADDRESS) {
+    throw new Error("NFT_CONTRACT_ADDRESS is invalid!");
+  }
+  const nftContractFactory = await hre.ethers.getContractFactory(
+    "NFT",
+    getAccount()
+  );
+  const nft = await hre.upgrades.upgradeProxy(
+    NFT_CONTRACT_ADDRESS,
+    nftContractFactory
+  );
   console.log(`Contract deployed to address: ${nft.address}`);
 });
 
@@ -72,11 +89,18 @@ task("token-uri", "Fetches the token metadata for the given token ID")
     );
   });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+// Tasks end
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.13",
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 20,
+      },
+    },
+  },
   networks: {
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
